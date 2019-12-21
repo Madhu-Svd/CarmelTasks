0337f440356c6266ecf3b134c5a3dda8afde134e
Madhu C Savadatti <madhu@carmelsolutions.net>
2019-12-21 17:38:08 +0530

    Android Updated

---
+++ Android/Login/app/src/main/java/com/carmel/login/ApprovalPeople.java
@@ -0,0 +1,14 @@
+package com.carmel.login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ApprovalPeople extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_approval_people);
+    }
+}
---
+++ Android/Login/app/src/main/java/com/carmel/login/RequisitionAttachment.java
@@ -0,0 +1,14 @@
+package com.carmel.login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RequisitionAttachment extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_requisition_attachment);
+    }
+}

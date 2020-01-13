03243cb44decb2df2be1d1a40343f1432c513342
Madhu C Savadatti <madhu@carmelsolutions.net>
2020-01-13 15:30:24 +0530

    Android-GuestJini Landing-Login Screens

---
+++ Android/GuestJini/app/src/main/java/com/carmel/guestjini/ForgotPasswordOTPValidation.java
@@ -0,0 +1,14 @@
+package com.carmel.guestjini;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ForgotPasswordOTPValidation extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_forgot_password_otpvalidation);
+    }
+}

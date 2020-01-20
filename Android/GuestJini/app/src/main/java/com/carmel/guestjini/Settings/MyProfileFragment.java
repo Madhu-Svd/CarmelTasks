0f9a69f784793df098d738ec6c5b04b6fb36ef87
Madhu C Savadatti <madhu@carmelsolutions.net>
2020-01-20 17:19:06 +0530

    Android updated

--- Android/GuestJini/app/src/main/java/com/carmel/guestjini/Settings/MyProfileFragment.java
+++ Android/GuestJini/app/src/main/java/com/carmel/guestjini/Settings/MyProfileFragment.java
@@ -15,7 +15,7 @@ import android.widget.ImageView;
 import com.carmel.guestjini.R;
 
 public class MyProfileFragment extends Fragment {
-    ImageView backIcon;
+    ImageView backIcon,profileToggleButton,gender,age,mobileNo,place,email;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -23,6 +23,14 @@ public class MyProfileFragment extends Fragment {
         // Inflate the layout for this fragment
         final View rootView=inflater.inflate(R.layout.fragment_my_profile, container, false);
         backIcon=rootView.findViewById(R.id.leftArrowMark);
+        profileToggleButton=rootView.findViewById(R.id.profileToggleButton);
+        gender=rootView.findViewById(R.id.genderToggleIcon);
+        age=rootView.findViewById(R.id.ageToggleIcon);
+        mobileNo=rootView.findViewById(R.id.mobileNoToggleIcon);
+        place=rootView.findViewById(R.id.yourPlaceToggleIcon);
+        email=rootView.findViewById(R.id.emailToggleIcon);
+
+
 
         backIcon.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -32,6 +40,97 @@ public class MyProfileFragment extends Fragment {
                 FragmentTransaction fragmentTransaction=fragmentManager.beginTransaction();
                 fragmentTransaction.replace(R.id.SettingsPlaceHolder,settingsLandingFragment);
                 fragmentTransaction.commit();
+            }
+        });
+
+        profileToggleButton.setOnClickListener(new View.OnClickListener() {
+            private boolean flag = true;
+            @Override
+            public void onClick(View v) {
+                if(flag){
+                    flag=false;
+                    profileToggleButton.setImageResource(R.drawable.toggle_icon_off_xxhdpi);
+                }else{
+                    flag=true;
+                    profileToggleButton.setImageResource(R.drawable.toggle_icon_on_xxhdpi);
+                }
+
+            }
+        });
+
+        gender.setOnClickListener(new View.OnClickListener() {
+            private boolean flag = true;
+            @Override
+            public void onClick(View v) {
+                if(flag){
+                    flag=false;
+                    gender.setImageResource(R.drawable.toggle_icon_off_xxhdpi);
+                }else{
+                    flag=true;
+                    gender.setImageResource(R.drawable.toggle_icon_on_xxhdpi);
+                }
+
+            }
+        });
+
+        mobileNo.setOnClickListener(new View.OnClickListener() {
+            private boolean flag = true;
+            @Override
+            public void onClick(View v) {
+                if(flag){
+                    flag=false;
+                    mobileNo.setImageResource(R.drawable.toggle_icon_on_xxhdpi);
+                }else{
+                    flag=true;
+                    mobileNo.setImageResource(R.drawable.toggle_icon_off_xxhdpi);
+                }
+
+            }
+        });
+
+        age.setOnClickListener(new View.OnClickListener() {
+            private boolean flag = true;
+            @Override
+            public void onClick(View v) {
+                if(flag){
+                    flag=false;
+                    age.setImageResource(R.drawable.toggle_icon_off_xxhdpi);
+                }else{
+                    flag=true;
+                    age.setImageResource(R.drawable.toggle_icon_on_xxhdpi);
+                }
+
+            }
+        });
+
+        place.setOnClickListener(new View.OnClickListener() {
+            private boolean flag = true;
+            @Override
+            public void onClick(View v) {
+                if(flag){
+                    flag=false;
+                    place.setImageResource(R.drawable.toggle_icon_on_xxhdpi);
+                }else{
+                    flag=true;
+                    place.setImageResource(R.drawable.toggle_icon_off_xxhdpi);
+                }
+
+            }
+        });
+
+
+        email.setOnClickListener(new View.OnClickListener() {
+            private boolean flag = true;
+            @Override
+            public void onClick(View v) {
+                if(flag){
+                    flag=false;
+                    email.setImageResource(R.drawable.toggle_icon_off_xxhdpi);
+                }else{
+                    flag=true;
+                    email.setImageResource(R.drawable.toggle_icon_on_xxhdpi);
+                }
+
             }
         });
         return rootView;
--- Android/GuestJini/app/src/main/java/com/carmel/guestjini/Support/MyTicketsRecyclerViewFragment.java
+++ Android/GuestJini/app/src/main/java/com/carmel/guestjini/Support/MyTicketsRecyclerViewFragment.java
@@ -3,6 +3,8 @@ package com.carmel.guestjini.Support;
 
 import android.os.Bundle;
 
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -10,6 +12,7 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import com.carmel.guestjini.R;
 
@@ -21,6 +24,8 @@ import Model.MyTicketsModel;
 public class MyTicketsRecyclerViewFragment extends Fragment {
 
     RecyclerView ticketsRecyclerView;
+    DrawerLayout drawerLayout;
+    ImageView ticketsFilterIcon;
     ArrayList<MyTicketsModel> myTicketsModelsList=new ArrayList<>();
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -28,6 +33,15 @@ public class MyTicketsRecyclerViewFragment extends Fragment {
         // Inflate the layout for this fragment
         final View rootView= inflater.inflate(R.layout.fragment_my_tickets_recycler_view, container, false);
         ticketsRecyclerView=rootView.findViewById(R.id.myTicketsRecyclerView);
+//        drawerLayout=rootView.findViewById(R.id.drawerLayout);
+//        ticketsFilterIcon=rootView.findViewById(R.id.filterIcon);
+//
+//        ticketsFilterIcon.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                drawerLayout.openDrawer(GravityCompat.START);
+//            }
+//        });
 
         LinearLayoutManager linearLayoutManager=new LinearLayoutManager(getContext());
         ticketsRecyclerView.setLayoutManager(linearLayoutManager);

610b51f3962bd4e435e54585f42dece815dd32d3
Madhu C Savadatti <madhu@carmelsolutions.net>
2020-01-25 16:15:19 +0530

    updated

---
+++ Android/app/src/main/java/com/carmel/materialactivity/SupportMaterialsActivity.java
@@ -0,0 +1,14 @@
+package com.carmel.materialactivity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SupportMaterialsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_support_materials);
+    }
+}

10e652270d810ebfc1446dcbf3f3dea5e0db99eb
Madhu C Savadatti <madhu@carmelsolutions.net>
2020-02-24 12:34:27 +0530

    android updated

--- Android/GuestJini/app/src/main/java/com/carmel/guestjini/MainActivity.java
+++ Android/GuestJini/app/src/main/java/com/carmel/guestjini/MainActivity.java
@@ -2,21 +2,25 @@ package com.carmel.guestjini;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
 
 import com.carmel.guestjini.R;
+import com.google.android.material.button.MaterialButton;
 import com.synnapps.carouselview.CarouselView;
 import com.synnapps.carouselview.ImageListener;
 
 public class MainActivity extends AppCompatActivity {
     CarouselView carouselView;
-    ImageView rightArrowIcon;
+    MaterialButton rightArrowIcon;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        rightArrowIcon=findViewById(R.id.landingButton);
         carouselView=findViewById(R.id.carouselView);
         final  int[] images=new int[]{
                 R.drawable.landing_image,R.drawable.orange_plain_image,R.drawable.purple_plain_image,R.drawable.yellow_plain_image
@@ -27,6 +31,14 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void setImageForPosition(int position, ImageView imageView) {
                 imageView.setImageResource(images[position]);
+            }
+        });
+
+        rightArrowIcon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(getApplicationContext(),Login.class);
+                startActivity(intent);
             }
         });
     }

e2fc4b115294b7a3c6a81285e591d9d279bd9c7c
Madhu C Savadatti <madhu@carmelsolutions.net>
2020-02-24 12:33:50 +0530

    Android updated

--- Android/GuestJini/app/src/main/java/com/carmel/guestjini/Login.java
+++ Android/GuestJini/app/src/main/java/com/carmel/guestjini/Login.java
@@ -18,7 +18,7 @@ import com.google.android.material.textfield.TextInputEditText;
 
 public class Login extends AppCompatActivity {
     TextView emailErrorField,passwordErrorField,forgotPassword,invalidCredentials,getOneNow;
-    TextInputEditText email,password;
+    EditText email,password;
     ImageView passwordIcon;
     MaterialButton loginButton;
     ConstraintLayout passwordLayout;
@@ -34,12 +34,12 @@ public class Login extends AppCompatActivity {
         forgotPassword=findViewById(R.id.forgotPassword);
         invalidCredentials=findViewById(R.id.invalidCredentials);
         getOneNow=findViewById(R.id.getOneNow);
-//        passwordIcon=findViewById(R.id.passwordMaskingIcon);
+        passwordIcon=findViewById(R.id.passwordMaskingIcon);
 
         email=findViewById(R.id.email);
-        password=findViewById(R.id.password_edit_text);
+        password=findViewById(R.id.password);
         loginButton=findViewById(R.id.login);
-//        passwordLayout=findViewById(R.id.passwordLayout);
+        passwordLayout=findViewById(R.id.passwordLayout);
 
 
         loginButton.setOnClickListener(new View.OnClickListener() {
@@ -48,30 +48,31 @@ public class Login extends AppCompatActivity {
                 if(email.getText().toString().trim().length()==0 && password.getText().toString().trim().length()==0){
                     emailErrorField.setVisibility(View.VISIBLE);
                     passwordErrorField.setVisibility(View.VISIBLE);
-//                    passwordLayout.setBackgroundResource(R.drawable.edit_red_textbox);
+                    passwordLayout.setBackgroundResource(R.drawable.edit_red_textbox);
+                    email.setBackgroundResource(R.drawable.edit_red_textbox);
                 }else {
                     emailErrorField.setVisibility(View.GONE);
                     passwordErrorField.setVisibility(View.GONE);
-//                    email.setBackgroundResource(R.drawable.edit_textbox);
-//                    passwordLayout.setBackgroundResource(R.drawable.edit_textbox);
+                    email.setBackgroundResource(R.drawable.edit_textbox);
+                    passwordLayout.setBackgroundResource(R.drawable.edit_textbox);
                     if(email.getText().toString().trim().length()==0){
                         emailErrorField.setVisibility(View.VISIBLE);
-//                        email.setBackgroundResource(R.drawable.edit_red_textbox);
+                        email.setBackgroundResource(R.drawable.edit_red_textbox);
                         invalidCredentials.setVisibility(View.GONE);
                     }else if(password.getText().toString().trim().length()==0){
                         passwordErrorField.setVisibility(View.VISIBLE);
-//                        passwordLayout.setBackgroundResource(R.drawable.edit_red_textbox);
+                        passwordLayout.setBackgroundResource(R.drawable.edit_red_textbox);
                         invalidCredentials.setVisibility(View.GONE);
                     }else if(!email.getText().toString().trim().matches(EMAIL_PATTERN) && !email.getText().toString().trim().matches(MobilePattern)){
                         invalidCredentials.setVisibility(View.VISIBLE);
                         emailErrorField.setVisibility(View.GONE);
-//                        email.setBackgroundResource(R.drawable.edit_textbox);
-//                        passwordLayout.setBackgroundResource(R.drawable.edit_textbox);
+                        email.setBackgroundResource(R.drawable.edit_textbox);
+                        passwordLayout.setBackgroundResource(R.drawable.edit_textbox);
                     }else if (password.getText().toString().trim().length()<8){
                         passwordErrorField.setVisibility(View.GONE);
                         invalidCredentials.setVisibility(View.VISIBLE);
-//                        email.setBackgroundResource(R.drawable.edit_textbox);
-//                        passwordLayout.setBackgroundResource(R.drawable.edit_textbox);
+                        email.setBackgroundResource(R.drawable.edit_textbox);
+                        passwordLayout.setBackgroundResource(R.drawable.edit_textbox);
                     }else {
                         Intent intent=new Intent(getApplicationContext(),ForgotPassword.class);
                         startActivity(intent);
@@ -80,6 +81,22 @@ public class Login extends AppCompatActivity {
             }
         });
 
+        passwordIcon.setOnClickListener(new View.OnClickListener() {
+            private boolean flag = true;
+            @Override
+            public void onClick(View v) {
+                if(flag) {
+                    flag=false;
+                    passwordIcon.setImageResource(R.drawable.password_unmasking_icon);
+                    password.setTransformationMethod(HideReturnsTransformationMethod.getInstance());
+
+                }else{
+                    flag=true;
+                    passwordIcon.setImageResource(R.drawable.password_masking_icon);
+                    password.setTransformationMethod(PasswordTransformationMethod.getInstance());
+                }
+            }
+        });
         forgotPassword.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
